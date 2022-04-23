a8b6a1605f105f9a010c1f28a85105b4af10e924
RevDonz <aprilio842@gmail.com>
2022-04-23 09:27:33 +0700

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace jurnal8_1302204051
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
